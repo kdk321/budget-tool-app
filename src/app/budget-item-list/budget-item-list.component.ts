9127fa8e92045d1a01423659ac18648c2b3755f6
Kent Koski <kent.koski@aa.com>
2019-12-12 22:08:12 -0700

    Added Item class and updated html to read from items array

--- src/app/budget-item-list/budget-item-list.component.ts
+++ src/app/budget-item-list/budget-item-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from '../models/item.model';
 
 @Component({
   selector: 'app-budget-item-list',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BudgetItemListComponent implements OnInit {
 
+  items: Item[] = [
+    {id: 1, paid: true, dateDue: new Date(), item: "Paycheck", amount: 500.00, reviewed: true},
+    {id: 2, paid: false, dateDue: new Date(), item: "Gas", amount: -28.00, reviewed: true}
+  ]
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
---
+++ src/app/models/item.model.ts
@@ -0,0 +1,10 @@
+import { CurrencyPipe } from '@angular/common';
+
+export class Item {
+    id: number;
+    paid: boolean;
+    dateDue: Date;
+    item: string;
+    amount: number;
+    reviewed: boolean;    
+}
